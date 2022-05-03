4c3e91322bc637c6debbacb7e5f0b547ab1a4d8e
xaukael <37848032+xaukael@users.noreply.github.com>
2022-05-03 11:04:51 -0500

    Add files via upload

---
+++ module.js
@@ -0,0 +1,14 @@
+Hooks.once('libWrapper.Ready', () => {
+libWrapper.register('multiline-chat-macros', 'Macro.prototype._executeChat', async function(...args) { 
+let messages = this.data.command.split('\n/').map((e, i)=>{ if (e.trim().at(0)!=="/" && i>0) return "/"+e; else return e;})
+  for (let m of messages)
+    ui.chat.processMessage(m).catch(err => {
+      Hooks.onError("Macro#_executeChat", err, {
+        msg: "There was an error in your chat message syntax.",
+        log: "error",
+        notify: "error",
+        command: this.data.command
+      });
+    });
+}, libWrapper.OVERRIDE);
+});
