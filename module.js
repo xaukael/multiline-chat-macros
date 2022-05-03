b6b40fcce202ab94b6e34ecceff74fa03f919a81
xaukael <37848032+xaukael@users.noreply.github.com>
2022-05-03 11:30:48 -0500

    Add files via upload

--- module.js
+++ module.js
@@ -1,5 +1,6 @@
-Hooks.once('libWrapper.Ready', () => {
-libWrapper.register('multiline-chat-macros', 'Macro.prototype._executeChat', async function(...args) { 
+Hooks.once('ready', () => {
+libWrapper.register('multiline-chat-macros', 'Macro.prototype.execute', async function(wrapped, ...args) { 
+if (this.data.type === "script") return wrapped(...args);
 let messages = this.data.command.split('\n/').map((e, i)=>{ if (e.trim().at(0)!=="/" && i>0) return "/"+e; else return e;})
   for (let m of messages)
     ui.chat.processMessage(m).catch(err => {
